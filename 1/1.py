4f963a4a13a1ae3b7a7be415949dcd210bc41860
Aram <me@aram.sh>
2024-11-30 23:57:05 -0800

    Initial commit

---
+++ 1/1.py
@@ -0,0 +1,27 @@
+with open('/Users/aram/Documents/Code/adventofcode/1.txt', 'r') as file:
+    lines = file.readlines()
+n = 0
+for line in range(len(lines)):
+    num1, num2 = lines[line].strip().split()
+
+    list1 = [int(digit) for digit in num1]
+    list2 = [int(digit) for digit in num2]
+
+    for i in range(len(list1)):
+        n += abs(list1[i] - list2[i])
+    
+print(n)
+left_numbers = []
+right_numbers = []
+
+for line in lines:
+    num1, num2 = map(int, line.strip().split())
+    left_numbers.append(num1)
+    right_numbers.append(num2)
+
+left_numbers.sort()
+right_numbers.sort()
+
+total_distance = sum(abs(l - r) for l, r in zip(left_numbers, right_numbers))
+
+print(total_distance)
---
+++ 1/2.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+with open('/Users/aram/Documents/Code/adventofcode/1/1.txt', 'r') as file:
+    lines = file.readlines()
+
+left_numbers = []
+right_numbers = []
+
+for line in lines:
+    num1, num2 = line.strip().split()
+    left_numbers.append(int(num1))
+    right_numbers.append(int(num2))
+
+right_counts = Counter(right_numbers)
+
+total = 0
+
+for num in left_numbers:
+    total += num * right_counts.get(num, 0)
+
+print(total)
